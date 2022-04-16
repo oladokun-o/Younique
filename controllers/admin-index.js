afe48de23ca74c6c0dfce4047f9568e836d13a20
Oladokun <65833243+honcho-man@users.noreply.github.com>
2022-04-16 20:29:38 +0100

    admin update

--- controllers/admin-index.js
+++ controllers/admin-index.js
@@ -1,13 +1,36 @@
 const programmingLanguages = require('../services/lan');
 
 module.exports = {
-    getLanguages: async function (req,res) {
+    getLanguages: async function (req, res) {
         try {
             res.json(await programmingLanguages.getMultiple(req.query.page));
           } catch (err) {
             res.status(500).redirect('error')
-            console.error(`Error while getting programming languages `, err.message);
-            next(err);
+            console.error(`Error while getting programming languages:`, err.message);            
           }
+    },
+    postLanguage: async function (req, res) {
+      try {
+        res.json(await programmingLanguages.create(req.body));
+      } catch (err) {
+        res.status(500).redirect('error')
+        console.error(`Error while creating programming language:`, err.message);        
+      }
+    },
+    updateLan: async function (req, res) {
+      try {
+        res.json(await programmingLanguages.update(req.params.id, req.body));
+      } catch (err) {
+        res.status(500).redirect('error')
+        console.error(`Error while updating programming language:`, err.message);        
+      }
+    },
+    deleteLan: async function (req, res) {
+      try {
+        res.json(await programmingLanguages.remove(req.params.id));
+      } catch (err) {
+        res.status(500).redirect('error')
+        console.error(`Error while deleting programming language:`, err.message);        
+      }
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -4,5 +4,8 @@ router.use(bodyParser.urlencoded({extended: true}));
 const control = require('../controllers/admin-index')
 router
     .get('/', control.getLanguages)
+    .post('/lan', control.postLanguage)
+    .put('/:id', control.updateLan)
+    .delete('/:id', control.deleteLan)
 
 module.exports = router;

26f91ecf142592769688c2f7ded9f4a4ac0ab5d3
Oladokun <oladokunoladipupo.gcc@gmail.com>
2022-05-03 23:06:09 +0100

    make it better!

--- controllers/client-index.js
+++ controllers/client-index.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     getHome: async function (req,res) {
-        res.render('client-home', {title: 'home'});
+        res.render('client-home', {title: 'Younique Store'});
     },
     getContact: async function (req,res) {
         res.render('client-contact', {title: 'contact'});

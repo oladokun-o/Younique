6132aef8f0e431ec9c01395a649ce314c5cbd5d3
Oladokun <65833243+honcho-man@users.noreply.github.com>
2022-06-09 10:34:07 +0100

    update

--- public/client/js/shop.js
+++ public/client/js/shop.js
@@ -2,7 +2,7 @@ $('.section-1').html(`
     <div class="col-md-6 col-12 p-md-3 product-search">
         <form class="form p-2">
             <input class="input px-2 py-0" type="text" placeholder="Search"/>
-            <button class="btn btn-md primary-button px-2 py-1">
+            <button class="btn btn-md primary-button px-2 py-1 col-1">
                 <i class="bi bi-search"></i>
             </button>
         </form>
